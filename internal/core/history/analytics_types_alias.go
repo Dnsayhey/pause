406cfc2f7d2e69bf610795ddd6ed1aca77bb6ca1
dnsayhey <yanlei921@live.com>
2026-03-25 15:30:04 +0800

    refactor(history): alias analytics models to core analytics dto

---
+++ internal/core/history/analytics_types_alias.go
@@ -0,0 +1,12 @@
+package history
+
+import coreanalytics "pause/internal/core/analytics"
+
+type AnalyticsReminderStat = coreanalytics.ReminderStat
+type AnalyticsSummaryStats = coreanalytics.SummaryStats
+type AnalyticsWeeklyStats = coreanalytics.WeeklyStats
+type AnalyticsSummary = coreanalytics.Summary
+type AnalyticsTrendPoint = coreanalytics.TrendPoint
+type AnalyticsTrend = coreanalytics.Trend
+type AnalyticsBreakTypeDistributionItem = coreanalytics.BreakTypeDistributionItem
+type AnalyticsBreakTypeDistribution = coreanalytics.BreakTypeDistribution
